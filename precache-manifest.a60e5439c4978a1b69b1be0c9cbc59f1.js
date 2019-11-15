9b4258c63191afb252efd7cd186cc02b59340c7e
Bobby <robertvaccaro@Bobbys-MBP.attlocal.net>
2019-11-14 22:06:28 -0800

    Updates

--- precache-manifest.a60e5439c4978a1b69b1be0c9cbc59f1.js
+++ precache-manifest.a60e5439c4978a1b69b1be0c9cbc59f1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Final-Portfolio/static/js/runtime~main.c6879fb2.js"
   },
   {
-    "revision": "d5c7dc1787437065d997",
-    "url": "/Final-Portfolio/static/js/main.d5c7dc17.chunk.js"
+    "revision": "b69825c826bc671cec4b",
+    "url": "/Final-Portfolio/static/js/main.b69825c8.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/Final-Portfolio/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "d5c7dc1787437065d997",
+    "revision": "b69825c826bc671cec4b",
     "url": "/Final-Portfolio/static/css/main.1e1d3a7d.chunk.css"
   },
   {
-    "revision": "b935dea051d4aa96cd57d98ea5ca0270",
+    "revision": "8ab76b4664db92a2e82382103ccb88da",
     "url": "/Final-Portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Final-Portfolio/precache-manifest.7298fdc2b3fc3a3dd42a740c247260d8.js"
+  "/Final-Portfolio/precache-manifest.a60e5439c4978a1b69b1be0c9cbc59f1.js"
 );
 
 workbox.clientsClaim();
