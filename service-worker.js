40379ea0e483a7bbdf459ba3750df1e35a46b241
Bobby <robertvaccaro@Bobbys-MacBook-Pro.local>
2019-11-27 08:49:29 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.robertvaccaro.com/precache-manifest.c7bb44722662e151cc9164a02cec8c4a.js"
+  "/Final-Portfolio/precache-manifest.81298f30b6a99b8deaf4152e1397cf27.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.robertvaccaro.com/index.html", {
+workbox.routing.registerNavigationRoute("/Final-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
